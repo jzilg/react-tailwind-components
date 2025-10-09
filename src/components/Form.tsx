feb091f0b2cb8004774bfd68df78c67606407ee1
Johannes Zilg <mail@jzilg.de>
2025-10-09 09:55:49 +0200

    remove unnecessary return from arrow function components

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,55 +2,45 @@ import type { FC, InputHTMLAttributes, SelectHTMLAttributes, TextareaHTMLAttribu
 
 type InputProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'type'>
 
-export const Input: FC<InputProps> = (props) => {
-  return (
-    <input
-      {...props}
-      className="py-2 px-3 border border-gray-500 rounded focus:outline-none focus-visible:ring focus-visible:ring-blue-500 ring-offset-1 disabled:opacity-50 disabled:bg-gray-200 disabled:hover:cursor-not-allowed"
-    />
-  )
-}
+export const Input: FC<InputProps> = (props) => (
+  <input
+    {...props}
+    className="py-2 px-3 border border-gray-500 rounded focus:outline-none focus-visible:ring focus-visible:ring-blue-500 ring-offset-1 disabled:opacity-50 disabled:bg-gray-200 disabled:hover:cursor-not-allowed"
+  />
+)
 
-export const Checkbox: FC<InputProps> = (props) => {
-  return (
-    <input
-      {...props}
-      type="checkbox"
-      className="size-6 accent-gray-500 hover:cursor-pointer focus:outline-none focus-visible:ring focus-visible:ring-blue-500 ring-offset-1 disabled:opacity-50 disabled:hover:cursor-not-allowed"
-    />
-  )
-}
+export const Checkbox: FC<InputProps> = (props) => (
+  <input
+    {...props}
+    type="checkbox"
+    className="size-6 accent-gray-500 hover:cursor-pointer focus:outline-none focus-visible:ring focus-visible:ring-blue-500 ring-offset-1 disabled:opacity-50 disabled:hover:cursor-not-allowed"
+  />
+)
 
-export const Radio: FC<InputProps> = (props) => {
-  return (
-    <input
-      {...props}
-      type="radio"
-      className="size-6 accent-gray-500 hover:cursor-pointer focus:outline-none focus-visible:ring focus-visible:ring-blue-500 ring-offset-1 disabled:opacity-50 disabled:hover:cursor-not-allowed"
-    />
-  )
-}
+export const Radio: FC<InputProps> = (props) => (
+  <input
+    {...props}
+    type="radio"
+    className="size-6 accent-gray-500 hover:cursor-pointer focus:outline-none focus-visible:ring focus-visible:ring-blue-500 ring-offset-1 disabled:opacity-50 disabled:hover:cursor-not-allowed"
+  />
+)
 
 type TextareaProps = TextareaHTMLAttributes<HTMLTextAreaElement>
 
-export const Textarea: FC<TextareaProps> = (props) => {
-  return (
-    <textarea
-      {...props}
-      className="py-2 px-3 border border-gray-500 rounded focus:outline-none focus-visible:ring focus-visible:ring-blue-500 ring-offset-1 disabled:opacity-50 disabled:bg-gray-200 disabled:hover:cursor-not-allowed"
-    />
-  )
-}
+export const Textarea: FC<TextareaProps> = (props) => (
+  <textarea
+    {...props}
+    className="py-2 px-3 border border-gray-500 rounded focus:outline-none focus-visible:ring focus-visible:ring-blue-500 ring-offset-1 disabled:opacity-50 disabled:bg-gray-200 disabled:hover:cursor-not-allowed"
+  />
+)
 
 type SelectProps = SelectHTMLAttributes<HTMLSelectElement>
 
-export const Select: FC<SelectProps> = ({ children, ...props}) => {
-  return (
-    <select
-      {...props}
-      className="py-2 px-3 border bg-transparent border-gray-500 hover:cursor-pointer rounded focus:outline-none focus-visible:ring focus-visible:ring-blue-500 ring-offset-1 disabled:opacity-50 disabled:bg-gray-200 disabled:hover:cursor-not-allowed"
-    >
-      {children}
-    </select>
-  )
-}
+export const Select: FC<SelectProps> = ({ children, ...props }) => (
+  <select
+    {...props}
+    className="py-2 px-3 border bg-transparent border-gray-500 hover:cursor-pointer rounded focus:outline-none focus-visible:ring focus-visible:ring-blue-500 ring-offset-1 disabled:opacity-50 disabled:bg-gray-200 disabled:hover:cursor-not-allowed"
+  >
+    {children}
+  </select>
+)
