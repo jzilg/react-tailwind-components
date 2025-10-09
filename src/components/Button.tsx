5b95515319fd4613f81603941e451c4980a9f958
Johannes Zilg <mail@jzilg.de>
2025-10-09 09:59:11 +0200

    upgrade to tailwind v4

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,7 @@ type Props = ButtonHTMLAttributes<HTMLButtonElement>
 export const Button: FC<Props> = ({ children, ...props }) => (
   <button
     {...props}
-    className="text-white py-2 px-5 bg-gray-500 hover:bg-gray-700 focus:outline-none focus-visible:ring focus-visible:ring-blue-500 ring-offset-1 rounded-full transition-colors disabled:opacity-50 disabled:hover:bg-gray-500 disabled:hover:cursor-not-allowed"
+    className="text-white py-2 px-5 bg-gray-500 hover:bg-gray-700 focus:outline-hidden focus-visible:ring-3 focus-visible:ring-blue-500 ring-offset-1 rounded-full transition-colors disabled:opacity-50 disabled:hover:bg-gray-500 disabled:hover:cursor-not-allowed"
   >
     {children}
   </button>
